1eb3360dadc59f65fbbb1b187aa22a3503d01ecb
apresannakumar <apresannakumar@paypal.com>
2017-05-20 12:52:38 +0530

    heroko ret

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ var secret='a6db367433edc83413205d5f464e0a7a';
 var reservation ={
 	test:function (req,res){
 		res.send('test page');
-	}
+	},
 	root:function (req,res){
 		console.log("asdfasdf");
 		var link ="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+"&redirect_uri="+encodeURI("https://wechat777.herokuapp.com/resp")+"&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
